fe4b3a453a274de9666825d1003a99e50e9b8661
Elizbeth <elizabethadebayo269@gmail.com>
2025-09-17 04:40:56 +0100

    created our product model and i made migrations

--- ecommerceApiProject/apiApp/models.py
+++ ecommerceApiProject/apiApp/models.py
@@ -9,5 +9,22 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
     
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    image = models.ImageField(upload_to='category_img', blank=True, null=True)
 
+    def __Str__(self):
+        return self.name
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField()
+    image = models.ImageField(upload_to="product_img", blank=True, null=True)
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __Str__(self):
+        return self.name
+    
